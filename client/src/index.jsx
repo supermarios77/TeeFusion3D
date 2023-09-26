cabd463b2ccf0bdf53bca14ab842dc902e1b5008
Mariya B <supermariyabros@gmail.com>
2023-09-26 20:23:03 +0300

    Update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './Styles.css'
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
