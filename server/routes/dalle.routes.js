02a9adfd73766365001b5604abdff2111828958e
Mariya B <supermariyabros@gmail.com>
2023-09-28 19:10:24 +0300

    Dalle doesnt work

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,10 +6,8 @@ dotenv.config();
 
 const router = express.Router();
 
-
-
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.VITE_OPENAI_API_KEY,
 });
 
 router.route('/').get((req, res) => {
